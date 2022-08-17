5f0ef788ddfaa6622c420908de926dfa21f0b001
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-08-17 20:57:36 +0300

    Fix: Response.ok & import function

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,18 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name'
 const searchParameters = 'fields=name,capital,population,flags,languages'
 
-export default function fetchCountries(name) {
+export function fetchCountries(name) {
     const url = `${BASE_URL}/${name}?${searchParameters}`;
     // console.log(url);
     return fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
         // .then(r => console.log(r))
-        .catch(error => console.log(error));
+        // .catch(error => console.log(error));
 }
 
 // export default { fetchCountries };
